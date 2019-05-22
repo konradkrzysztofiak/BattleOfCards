faf0408a39b0aec6ad35cf79d2905f5759b63eda
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-05-22 22:06:42 +0200

    Create player class

---
+++ src/com/codecool/klondike/Player.java
@@ -0,0 +1,36 @@
+package com.codecool.klondike;
+
+public class Player {
+    //private String nickname;
+    private int id;
+    private boolean turn = false;
+    private int score = 0;
+    private boolean isPlayerAllive;
+
+    public Player( int id, int score){
+        //this.nickname = nickname;
+        this.id = id;
+        this.score = score;
+        this.isPlayerAllive = true;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public boolean hasTurn() {
+        return turn;
+    }
+
+    public void setPlayerAllive(boolean playerAllive) {
+        isPlayerAllive = playerAllive;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
